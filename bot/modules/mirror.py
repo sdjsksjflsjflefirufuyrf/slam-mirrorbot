9ee88542205c6cab9e52ca34b6c8cc8b3825dcb7
DJG <91832013+sdjsksjflsjflefirufuyrf@users.noreply.github.com>
2021-10-04 02:53:44 +0600

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -244,7 +244,7 @@ class MirrorListener(listeners.MirrorListeners):
                 update_all_messages()
             return
         with download_dict_lock:
-            msg = f'<b>Filename: </b><code>{download_dict[self.uid].name()}</code>\n<b>Size: </b><code>{size}</code>\n'
+            msg = f'<b>Filename: </b><code>{download_dict[self.uid].name()}</code>\n\n<b>Size: </b><code>{size}</code>\n'
             if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
                 msg += '\n<b>Type: </b><code>Folder</code>\n'
                 msg += f'\n<b>SubFolders: </b><code>{folders}</code>\n'
